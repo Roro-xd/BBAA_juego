41fb4de4f7f1aa21ff8703e6f104c016323733cf
Roro-xd <rocioloportcuarto@gmail.com>
2025-05-13 14:45:22 +0200

    BaseInicio

--- Assets/Scripts/UI/Inicio/Script_inicio.cs
+++ Assets/Scripts/UI/Inicio/Script_inicio.cs
@@ -1,13 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Script_inicio : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    GameObject panelInicio;
+    GameObject panelAjustes;
+    GameObject panelExtras;
+    GameObject panelCreditos;
+
+
     void Start()
     {
+        panelInicio = GameObject.Find("Panel_inicio");
         
+        panelAjustes = GameObject.Find("Panel_ajustes");
+        panelAjustes.SetActive(false);
+
+        panelExtras = GameObject.Find("Panel_extras");
+        panelExtras.SetActive(false);
+
+        panelCreditos = GameObject.Find("Panel_creditos");
+        panelCreditos.SetActive(false);
     }
 
    
@@ -15,4 +31,56 @@ public class Script_inicio : MonoBehaviour
     {
         
     }
+
+
+    //Para los botones de la pantalla de inicio
+    public void botonInicioSalir() {
+        Application.Quit();
+    }
+
+
+    public void botonInicioJugar() {
+        SceneManager.LoadScene("Lobby"); //CAMBIAR A NOMBRE QUE VAYA A TENER AL FINAL
+    }
+
+    
+    public void botonInicioAjustes() {
+        panelAjustes.SetActive(true);
+    }
+
+
+    public void botonInicioCreditos() {
+        panelCreditos.SetActive(true);
+    }
+
+
+    //Para los botones de la pantalla de ajustes
+
+        //PONER LAS COSAS DEL VOLUMEN
+        
+        //Para los botones de la pantalla de extras
+        public void botonAjustesExtras() {
+            panelExtras.SetActive(true);
+        }
+
+        public void botonExtrasVolver() {
+            panelExtras.SetActive(false);
+        }
+
+    public void botonAjustesCreditosVolver() {
+        /*if (panelAjustes.SetActive(true) == true) {
+            panelAjustes.SetActive(false);
+        };
+
+        if (panelCreditos.SetActive(true) == true) {}
+            panelCreditos.SetActive(false);
+        };*/
+        panelAjustes.SetActive(false);
+        panelCreditos.SetActive(false);
+    }
+
+
+    //AÑADIR SONIDOS DE LOS BOTONES
+
+
 }
