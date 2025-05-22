cc92e4173ba0ad333d355488898bc49062d86fce
Roro-xd <rocioloportcuarto@gmail.com>
2025-05-22 19:54:38 +0200

    Derrota

---
+++ Assets/Scripts/UI/Script_derrota.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Script_derrota : MonoBehaviour
+{
+
+    //ME FALTA QUITAR LA MÚSICA DEL INICIO
+    //EN ESTA ESCENA NO SONARÍA NADA, CREO QUE ASÍ LE DA UN TOQUE TODAVÍA MÁS TRISTE
+
+    //Referenciar al panel negro
+    public GameObject panelNegro;
+    private Animator animPanelNegro;
+
+    //Referenciar los botones que salen al final
+    public GameObject botonReintentar;
+    public GameObject botonSalir;
+
+    //Referenciar el cuadre de fondo del texto
+    public GameObject cuadroTexto;
+
+    //Referenciar los botones indicadores de interaccion dialogo
+    public GameObject zRegresa;
+    public GameObject xAvanza;
+
+    //Crear y ordenar las lineas del monologo
+    public GameObject[] monologo;
+    public int lineaMonologo = 0;
+
+
+
+    void Start()
+    {
+        //Que encuentre el Animator del panel negro
+        animPanelNegro = panelNegro.GetComponent<Animator>();
+        
+        //El texto aparece una vez que el panel negro no está y se ve toda la escena
+        StartCoroutine("aparicionTexto");
+        
+    }
+
+    void Update()
+    {
+        //No te permite regresar el texto nada más empieza
+        if (lineaMonologo == 0 )
+        {
+            zRegresa.SetActive(false);
+            xAvanza.SetActive(true);
+        }
+        else
+        { 
+            zRegresa.SetActive(true);
+            xAvanza.SetActive(true);
+        }
+
+
+        //Al presionar la tecla indicada(X)
+        if (Input.GetKeyDown(KeyCode.X) && xAvanza.activeSelf)
+        {
+            //Si no es la ultima linea, el texto avanza
+            if (lineaMonologo != 2)
+            {
+                AvanceMonologo();
+                AudioManager.Instance.PlaySFX("Botones");
+            }
+            //Si es la ultima linea, reaparece el panel negro, se desactiva el texto y al tiempo aparecen los botones finales
+            else
+            {
+                animPanelNegro.SetBool("Panel_in", true);
+                cuadroTexto.SetActive(false);
+                monologo[lineaMonologo].SetActive(false);
+                StartCoroutine("aparicionBotones");
+            }
+        }
+
+
+        //Al presionar la tecla indicada(Z)
+        if (Input.GetKeyDown(KeyCode.Z) && zRegresa.activeSelf)
+        {
+            //Retroce una linea del monologo
+            RetrocesoMonologo();
+            AudioManager.Instance.PlaySFX("Volver");
+
+        }
+        //Da error si se intenta retroceder durante el monologo y la tecla no se indica
+        else if (Input.GetKeyDown(KeyCode.Z) && zRegresa.activeSelf == false && cuadroTexto.activeSelf)
+        {
+            AudioManager.Instance.PlaySFX("Error");
+        }
+
+    }
+
+
+
+    public void AvanceMonologo()
+    {
+        monologo[lineaMonologo].SetActive(false);
+        lineaMonologo = (lineaMonologo + 1) % monologo.Length;
+        monologo[lineaMonologo].SetActive(true);
+    }
+
+
+    public void RetrocesoMonologo()
+    { 
+        monologo[lineaMonologo].SetActive(false);
+        lineaMonologo--;
+            if (lineaMonologo < 0)
+            {
+                lineaMonologo += monologo.Length;
+            }
+
+            monologo[lineaMonologo].SetActive(true);
+    }
+    
+    
+    //Tiempo de espera para que aparezcan los bonotes finales
+    IEnumerator aparicionBotones()
+    {
+        yield return new WaitForSeconds(2);
+        botonReintentar.SetActive(true);
+        botonSalir.SetActive(true);
+    }
+
+
+    //Tiempo de espera para que aparezca el texto al iniciar la escena
+    IEnumerator aparicionTexto()
+    {
+        yield return new WaitForSeconds(1);
+        cuadroTexto.SetActive(true);
+        monologo[0].SetActive(true);
+    }
+
+
+    //Funciones para los botones del final
+    public void SalirJuego()
+    {
+        Application.Quit();
+    }
+
+    public void Reintentar()
+    {
+        SceneManager.LoadScene("Inicio");
+    }
+    
+    
+}
