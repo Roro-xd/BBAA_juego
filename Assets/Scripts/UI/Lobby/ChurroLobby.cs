fab54f983216123ab6c30dc5edd4ec691cb0ecd5
Roro-xd <rocioloportcuarto@gmail.com>
2025-05-30 14:46:02 +0200

    inicio mecanica relaciones

--- Assets/Scripts/UI/Lobby/ChurroLobby.cs
+++ Assets/Scripts/UI/Lobby/ChurroLobby.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class ChurroLobby : MonoBehaviour
 {
+
+    //Líneas de conversación de Churro
+    public GameObject[] textosChurro;
+    public int lineaChurro = 1;
+
+
     public GameObject xChurro;
     public GameObject baseTexto;
-    public GameObject textoChurro;
+    public GameObject textoChurro; //Probablemente no haga falta luego
     public GameObject dialogoChurro;
 
 
@@ -18,9 +24,13 @@ public class ChurroLobby : MonoBehaviour
     private bool puedoConversar;
 
 
+    //hacer referencia al script de Relaciones para poder mencionar el nivel de Churro
+
+
 
     void Start()
     {
+        textosChurro[lineaChurro].SetActive(false);
         xChurro.SetActive(false);
     }
 
@@ -32,7 +42,7 @@ public class ChurroLobby : MonoBehaviour
             puedoConversar = true;
         }
         else
-        { 
+        {
             puedoConversar = false;
         }
 
@@ -40,21 +50,43 @@ public class ChurroLobby : MonoBehaviour
 
         if (xChurro.activeSelf && Input.GetKeyDown(KeyCode.X) && puedoConversar)
         {
+            //AvanceTextoYori();
             baseTexto.SetActive(true);
-            textoChurro.SetActive(true);
+            textosChurro[lineaChurro].SetActive(true);
             dialogoChurro.SetActive(true);
 
             xChurro.SetActive(false);
             AudioManager.Instance.PlaySFX("Botones");
         }
-        else if (textoChurro.activeSelf && Input.GetKeyDown(KeyCode.X) && puedoConversar)
+        else if (Input.GetKeyDown(KeyCode.X) && puedoConversar && lineaChurro == 0)
         {
-            baseTexto.SetActive(false);
-            textoChurro.SetActive(false);
-            dialogoChurro.SetActive(false);
-            AudioManager.Instance.PlaySFX("Volver");
-        }
+            //APARICION DE PANEL/IMAGEN+BOTONES PARA ESCOGER
 
+            AudioManager.Instance.PlaySFX("Botones");
+        }
+        /*else if (Input.GetKeyDown(KeyCode.X) && puedoConversar && lineaChurro == 1)
+        {
+            AvanceTextoChurro();
+            baseTexto.SetActive(false);
+            textosChurro[lineaChurro].SetActive(false);
+            AudioManager.Instance.PlaySFX("Volver");
+        }*/
+        else if (Input.GetKeyDown(KeyCode.X) && puedoConversar && (lineaChurro == 1 || lineaChurro == 2))
+        {
+            textosChurro[lineaChurro].SetActive(false);
+            lineaChurro = 3;
+            dialogoChurro.SetActive(false);
+            baseTexto.SetActive(false);
+        }
+    }
+
+
+
+    public void AvanceTextoChurro()
+    {
+        textosChurro[lineaChurro].SetActive(false);
+        lineaChurro = (lineaChurro + 1) % textosChurro.Length;
+        textosChurro[lineaChurro].SetActive(true);
     }
 
 
@@ -65,10 +97,28 @@ public class ChurroLobby : MonoBehaviour
             xChurro.SetActive(true);
         }
     }
-    
+
 
     void OnTriggerExit2D(Collider2D col)
     {
         xChurro.SetActive(false);
+    }
+
+    public void Eleccion1Churro()
+    {
+        //cerrar panel de elección
+
+        lineaChurro = 1;
+        //Establecer puntuación
+        //relaciones.nivelChurro -= 1;
+    }
+
+    public void Eleccion2Churro()
+    {
+        //cerrar panel de elección
+
+        lineaChurro = 1;
+        //Establecer puntuación
+        //relaciones.nivelChurro += 0;
     }
 }
---
+++ Assets/Scripts/UI/Relaciones.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Relaciones : MonoBehaviour
+{
+    public const int nivelChurro = 0;
+    public const int nivelNapo = 0;
+    public const int nivelYori = 0;
+
+    public TextMeshProUGUI textoNivelCh;
+    public TextMeshProUGUI textoNivelN;
+    public TextMeshProUGUI textoNivelY;
+
+    public GameObject relChurro;
+    public GameObject relNapo;
+    public GameObject relYori;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
